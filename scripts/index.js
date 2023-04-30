be6845651e53fb3a23559b88f339d26e645bdd10
Your Name <you@example.com>
2023-04-30 10:16:40 +0300

    Сверстана адаптивная версия сайта

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const ButtonPopupOpen = document.querySelector('.profile__button_type_edit');
+const popup = document.querySelector('.popup');
+const buttonPopupClose = document.querySelector('.popup__close');
+
+
+
+
+console.log ({
+    ButtonPopupOpen,
+    popup,
+    buttonPopupClose
+})
