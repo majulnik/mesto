1ea7951aca6cdfe2cb32bf1fc362dbc4917abab0
Your Name <you@example.com>
2023-04-30 12:35:51 +0300

    feat: добавлена форма редактирования профиля с js

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,10 @@ const ButtonPopupOpen = document.querySelector('.profile__button_type_edit');
 const popup = document.querySelector('.popup');
 const buttonPopupClose = document.querySelector('.popup__close');
 
+ButtonPopupOpen.addEventListener('click', () => {
+    popup.classList.add('popup__opened');
+});
 
-
-
-console.log ({
-    ButtonPopupOpen,
-    popup,
-    buttonPopupClose
-})
+buttonPopupClose.addEventListener('click', () => {
+    popup.classList.remove('popup__opened');
+});
