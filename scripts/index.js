5cd5da19f00abf6d9130ce7301f749e2657c6577
Your Name <you@example.com>
2023-05-17 16:33:17 +0300

    feat: добавлен попап изображения галереи

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,13 @@
 const initialCards = [
   {
     name: 'Байкал',
-    link: 'https://images.unsplash.com/photo-1652167934538-c0b4ab5ced1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    link: 'https://images.unsplash.com/photo-1564324738343-a8aeafb375d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fCVEMCVCMCVEMCVCQiVEMSU4MiVEMCVCMCVEMCVCOXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60',
     alt: 'Фото Байкал',
     like: false,
   },
   {
     name: 'Дагестан',
-    link: 'https://images.unsplash.com/photo-1627327660729-a48ba87c4bb5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+    link: 'https://images.unsplash.com/photo-1643281237857-5f14c2b9f3ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8JUQwJUIwJUQwJUJCJUQxJTgyJUQwJUIwJUQwJUI5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
     alt: 'Фото Дагестан',
     like: false,
   },
@@ -19,15 +19,15 @@ const initialCards = [
     like: false,
   },
   {
-    name: 'Карелия',
-    link: 'https://images.unsplash.com/photo-1575582293156-7d185b60c7bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    alt: 'Фото Карелия',
-    like: false,
-  },
-  {
     name: 'Санкт-Петербург',
     link: 'https://images.unsplash.com/photo-1597533849860-5a04a21a7b3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
     alt: 'Фото Санкт-Петербург',
+    like: false,
+  },
+  {
+    name: 'Карелия',
+    link: 'https://images.unsplash.com/photo-1575582293156-7d185b60c7bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    alt: 'Фото Карелия',
     like: false,
   },
   {
@@ -45,7 +45,6 @@ const jobInput = formElement.querySelector('#profile-description');
 
 const placeInput = document.querySelector('#new-place');
 const linkInput = document.querySelector('#new-link');
-console.log (placeInput, linkInput)
 
 const profileNameElement = document.querySelector('.profile__name');
 const profileDescriptionElement = document.querySelector('.profile__description');
@@ -60,6 +59,7 @@ const buttonItemPopupClose = document.getElementById('closeItemPopup');
 const buttonItemPopupOpen = document.getElementById('openItemPopup');
 const buttonItemPopupSave = document.getElementById('saveItem');
 
+// Добавление информации о пользователе на страницу через попап
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
@@ -72,7 +72,7 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
-// Добавление новой карточки на страницу
+// 3. Добавление карточки - Добавление элемента в начало массива: метод unshift
 function handleItemAdd(evt) {
   evt.preventDefault();
   closeItemPopup();
@@ -89,8 +89,6 @@ function handleItemAdd(evt) {
   };
   initialCards.unshift(newItem);
   displayElements(initialCards);
-
-
 }
 
 function openPopup() {
@@ -119,13 +117,12 @@ buttonPopupClose.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-// 2. Открытие и закрытие попапа с добавлением места
+// 2. Форма добавления карточки - открытие нажатием на кнопку «+» и закрытие кликом на крестик
 buttonItemPopupOpen.addEventListener('click', openItemPopup);
 
 buttonItemPopupClose.addEventListener('click', closeItemPopup);
 
 buttonItemPopupSave.addEventListener('click', handleItemAdd);
-
 
 function displayElements(cards) {
   const elementsContainer = document.getElementById('elements__container');
@@ -145,32 +142,33 @@ function displayElements(cards) {
     card.innerHTML = content;
     elementsContainer.appendChild(card);
 
-    //4. Лайк карточки - работает только на ПЕРВОЙ карточке
-    const likeItem = card.querySelector('.elements__like');
-    console.log(likeItem);
-    likeItem.addEventListener('click', () => {
-      likeItem.classList.add('elements__like_active');
-
+// 6. Открытие попапа с картинкой
+const imageItem = document.querySelector('.elements__image');
+const imageOpen = document.getElementById('gallery_popup');
+const imageItemClose = document.querySelector('.popup__close');
     
-  });
-    console.log(elementsContainer);
+imageItem.addEventListener('click', () => {
+    imageOpen.classList.add('popup__opened');   
+});
 
+imageItemClose.addEventListener('click', () => {
+  imageOpen.classList.remove('popup__opened');
+});
 
+// 4. Лайк карточки
+const likeItem = card.querySelector('.elements__like');
+likeItem.addEventListener('click', () => {
+likeItem.classList.add('elements__like_active');
+
+});
 
 });
   };
   displayElements(initialCards);
 
 
-// 2. Форма добавления карточки - открытие нажатием на кнопку «+» и закрытие кликом на крестик
-
-// 3. Добавление карточки - Добавление элемента в начало массива: метод unshif???
 
 
 
 // 5. Удаление карточки через иконку корзины
 
-// 6. Открытие попапа с картинкой
-
-
-// Если перезагрузить страницу, карточки и лайки не сохранятся.
